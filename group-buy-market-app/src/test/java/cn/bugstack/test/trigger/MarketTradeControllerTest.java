a4cec07ae3bade8865751f86d3a6957939e81bb6
fuzhengwei <184172133@qq.com>
2025-01-25 19:56:36 +0800

    feat: 第2-11节：交易规则责任链过滤

--- group-buy-market-app/src/test/java/cn/bugstack/test/trigger/MarketTradeControllerTest.java
+++ group-buy-market-app/src/test/java/cn/bugstack/test/trigger/MarketTradeControllerTest.java
@@ -30,7 +30,7 @@ public class MarketTradeControllerTest {
     @Test
     public void test_lockMarketPayOrder() {
         LockMarketPayOrderRequestDTO lockMarketPayOrderRequestDTO = new LockMarketPayOrderRequestDTO();
-        lockMarketPayOrderRequestDTO.setUserId("xiaofuge");
+        lockMarketPayOrderRequestDTO.setUserId("xfg02");
         lockMarketPayOrderRequestDTO.setTeamId(null);
         lockMarketPayOrderRequestDTO.setActivityId(100123L);
         lockMarketPayOrderRequestDTO.setGoodsId("9890001");
