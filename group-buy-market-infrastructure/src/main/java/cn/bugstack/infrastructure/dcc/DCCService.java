d04077245100b42541e6ddbedff2ae01cf54634a
fuzhengwei <184172133@qq.com>
2025-01-30 16:17:57 +0800

    feat: 第2-13节：交易结算责任链过滤

--- group-buy-market-infrastructure/src/main/java/cn/bugstack/infrastructure/dcc/DCCService.java
+++ group-buy-market-infrastructure/src/main/java/cn/bugstack/infrastructure/dcc/DCCService.java
@@ -1,8 +1,13 @@
 package cn.bugstack.infrastructure.dcc;
 
 import cn.bugstack.types.annotations.DCCValue;
+import cn.bugstack.types.common.Constants;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
@@ -21,6 +26,9 @@ public class DCCService {
     @DCCValue("cutRange:100")
     private String cutRange;
 
+    @DCCValue("scBlacklist:s02c02")
+    private String scBlacklist;
+
     public boolean isDowngradeSwitch() {
         return "1".equals(downgradeSwitch);
     }
@@ -38,6 +46,14 @@ public class DCCService {
         }
 
         return false;
+    }
+
+    /**
+     * 判断黑名单拦截渠道，true 拦截、false 放行
+     */
+    public boolean isSCBlackIntercept(String source, String channel) {
+        List<String> list = Arrays.asList(scBlacklist.split(Constants.SPLIT));
+        return list.contains(source + channel);
     }
 
 }
